8a72c39838c6019e1610ff161e53a09e601842ff
Juan <luis240401@gmail.com>
2022-06-07 11:47:42 -0500

    first commit

---
+++ routes/languages.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+const languages = require('../services/languages');
+
+/* GET languages listing. */
+router.get('/', async function(req, res, next) {
+    try{
+        res.json(await languages.getMultiple(req.query.page));
+    }catch(err){
+        console.error('Error' + err.message);
+        next(err);
+    }
+});
+
+router.get('/permissions', function(req, res, next) {
+  res.render("permissions");
+});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  //res.send('respond with a resource');
+  res.render("list", { title: "some text"});
+});
+
+router.get('/permissions', function(req, res, next) {
+  res.render("permissions");
+});
+
+router.get('/json-list', async function(req, res, next) {
+  //res.send('respond with a resource');
+  const users = require('../services/languages');
+  try{
+    res.json(await users.getUser(req.query.page));
+  }catch(err){
+    console.error('Error' + err.message);
+    next(err);
+  }
+});
+
+module.exports = router;
