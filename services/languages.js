a355c8bc16f33c67fd7697e7fba903e7c5d0bb4e
Juan <luis240401@gmail.com>
2022-06-13 18:51:24 -0500

    Second commit

--- services/languages.js
+++ services/languages.js
@@ -37,6 +37,46 @@ async function getUser(page = 1){
     }
 }
 
+async function create(language){
+
+    const result = await db.query(
+        `INSERT INTO languages (name, description, year) VALUES
+        ('${language.name}', '${language.description}', ${language.year})`
+    );
+
+    let message = "Error in creating a language";
+    if(result.affectedRows){
+        message = "A new language has been added";
+    }
+
+    return{ message }
+
+}
+
+async function update(id, language){
+
+    const result = await db.query(
+        `UPDATE languages
+        SET 
+        name = ('${language.name}', 
+        description = '${language.description}', 
+        year = ${language.year})
+        WHERE id = ${id}
+        `
+    );
+
+    let message = "Error in updating a language";
+    if(result.affectedRows){
+        message = "A language has been updated";
+    }
+
+    return{ message }
+
+}
+
 module.exports = {
-    getUser, getMultiple
+    getUser, 
+    getMultiple, 
+    create,
+    update
 };
